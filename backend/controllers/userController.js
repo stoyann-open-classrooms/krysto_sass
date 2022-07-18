179c9fd9886740c75845406896812d330e7360c9
Stoyann <stoyann.openclassrooms@gmail.com>
2022-07-18 18:24:29 +1100

    authMiddleware et protectRoutes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,6 +68,22 @@ const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route')
 })
 
+
+// @desc Get current User
+// @route /api/users/me
+// @ access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+res.status(200).json(user)
+  res.send('me')
+})
+
+
+
 // Generate token 
 const generateToken = (id) => {
   return jwt.sign({id}, process.env.JWT_SECRET, {
@@ -78,4 +94,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
