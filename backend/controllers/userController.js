f3935b2ffbbca9cc5e0d51b8a47a169fdffb8fcf
Stoyann <stoyann.openclassrooms@gmail.com>
2022-07-18 17:58:12 +1100

    login et jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
@@ -34,6 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400)
@@ -46,8 +48,32 @@ const registerUser = asyncHandler(async (req, res) => {
 // @ access Public
 
 const loginUser = asyncHandler(async (req, res) => {
+  const {email, password} = req.body
+  const user = await User.findOne({email})
+  
+  // check user and password match
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(401)
+    throw new Error(' impossible de ce connecter email ou password incorect ! ')
+ }
+ 
+ 
   res.send('Login Route')
 })
+
+// Generate token 
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
