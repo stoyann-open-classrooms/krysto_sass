ad755785cf2a32f047d4040ca590a0b4917a2c97
Stoyann <stoyann.openclassrooms@gmail.com>
2022-07-18 16:46:17 +1100

    route users et middleware error

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('server...');
+//dependances
+const express = require('express')
+const dotenv = require('dotenv').config()
+//midlleware
+const {errorHandler } = require ('./middleware/errorMiddleware')
+
+// initialisation
+const PORT = process.env.PORT
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).send("Bienvenue sur l'api de krysto")
+})
+
+//Routes
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
